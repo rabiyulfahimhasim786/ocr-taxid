6754d13cbca5a101e01b6e750e437f82dfc9806e
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2022-12-05 05:36:12 -0600

    Update settings.py

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,17 +121,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+#STATIC_URL = '/static/'
+#MEDIA_URL = '/images/'
+#
+#STATICFILES_DIRS = [
+#    BASE_DIR / 'static'
+#]
 
 #MEDIA_ROOT = BASE_DIR / 'static/images'
-MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#MEDIA_ROOT = BASE_DIR / 'media'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+#
+#apache server media and static files configurations
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #github code
 # AWS_QUERYSTRING_AUTH = False
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
